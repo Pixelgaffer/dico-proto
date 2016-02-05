527cef18f19eabfb4a47c07ff01a3b3c7ae36a8d
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-02-05 22:47:01 +0100

    use master branch for go code

--- update_protos.sh
+++ update_protos.sh
@@ -1,14 +1,6 @@
 set -e
-git checkout master
 LATEST_COMMIT_MSG=`git log -1 --pretty=%B`
-TMPDIR=`mktemp -d -t protosXXXXXX`
-echo "TMPDIR: $TMPDIR"
-cp ./*.proto $TMPDIR
-git checkout go
-protoc --go_out=. --proto_path=$TMPDIR $TMPDIR/*.proto
+protoc --go_out=. *.proto
 echo "commiting code"
 git add *.pb.go
 git commit -m "sync: $LATEST_COMMIT_MSG"
-git checkout master
-echo "deleting $TMPDIR"
-rm -rf $TMPDIR
