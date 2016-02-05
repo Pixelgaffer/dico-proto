aba62ea6c388b17ec5620d1a03c92960e30f9f2d
Mrmaxmeier <Mrmaxmeier@gmail.com>
2016-02-05 21:37:04 +0100

    add update_protos.sh

---
+++ update_protos.sh
@@ -0,0 +1,15 @@
+set -e
+git checkout master
+LATEST_COMMIT_MSG=`git log -1 --pretty=%B`
+TMPDIR=`mktemp -d -t protosXXXXXX`
+echo "TMPDIR: $TMPDIR"
+cp ./*.proto $TMPDIR
+git checkout go
+protoc --go_out=. --proto_path=$TMPDIR $TMPDIR/*.proto
+echo "commiting code"
+git add *.pb.go
+git commit -m "sync: $LATEST_COMMIT_MSG"
+git push
+git checkout master
+echo "deleting $TMPDIR"
+#rm -rf $TMPDIR
